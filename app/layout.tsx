b9aa3a5c8844daf28599400189ca22978b590cdc
miri-san <mihir@velt.dev>
2026-05-05 15:18:13 +0530

    Added superflow script

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,12 @@ export default function RootLayout({
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Urbanist:wght@300;400;500;600;700&family=Fira+Mono:wght@400;500;700&family=Fira+Code:wght@400;500;600&family=Poppins:wght@400;500;600&family=Geist+Mono:wght@400;500;600;700&display=swap"
         />
+        <script
+          id="superflowToolbarScript"
+          data-sf-platform="other-manual"
+          async
+          src="https://cdn.velt.dev/lib/superflow.js?apiKey=aU1MxKP0rca2UXwKi8bl&projectId=620866069199868"
+        />
       </head>
       <body>{children}</body>
     </html>
